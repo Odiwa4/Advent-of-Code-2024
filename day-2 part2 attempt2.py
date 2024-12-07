d79a040b1a7bccacc8f691492ebf2429c59355b0
OdiwaDev <oliver.v.thornton+github@gmail.com>
2024-12-07 10:55:07 +1000

    day 2 finished

--- day-2 part2 attempt2.py
+++ day-2 part2 attempt2.py
@@ -9,28 +9,50 @@ with open (path, 'r') as d:
         reports[i] = [int(item) for item in reports[i]]
     d.close()
 
-def CheckSafe(list):
+def GetWrong(report):
     #EITHER ALL DECREASING OR ALL INCREASING
     #NUMBERS DONT INCREASE/DECREASE MORE THAN 3
     #IF ONE NUMBER CAN BE REMOVED TO FIX IT, IT PASSES
-    
+    wrongIndexes = []
     lastDifference = 0
-    for i in range(len(list)):
-        increasing = True
-        if (i == len(list) - 1):
+    for i in range(len(report)):
+        if (i == len(report) - 1):
             break
         
         #all increasing / all decreasing
-        difference = list[i] - list[i+1]
+        difference = report[i] - report[i+1]
         if (difference > 3 or difference < -3 or difference == 0):
-            return False
+            wrongIndexes.append(i)
+            wrongIndexes.append(i+1)
         if (lastDifference != 0):
             if ((lastDifference > 0 and difference < 0) or (lastDifference < 0 and difference > 0)):
-                return False
+                wrongIndexes.append(i)
+                wrongIndexes.append(i+1)
         lastDifference = difference
-    return True
 
-print(CheckSafe([1, 3, 6, 7, 8]))
+    wrongIndexes = list(dict.fromkeys(wrongIndexes.copy()))
+    return wrongIndexes
+
+def CheckSafe(report):
+    wrong = GetWrong(report)
+    print(wrong)
+    for i in range(len(wrong)):
+        newList = []
+        for x in range(len(report)):
+            if (x != wrong[i]):
+                newList.append(report[x])
+        if (len(GetWrong(newList)) == 0):
+            return True
+    firstList = []
+    for x in range(len(report)):
+        if (x != 0):
+            firstList.append(report[x])
+    if (len(GetWrong(firstList)) == 0):
+        return True
+    else:
+        return False
+    
+#print(CheckSafe([48, 46, 47, 49, 51, 54, 56]))
 
 safeCount = 0
 for r in range(len(reports)):
